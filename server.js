a5b94e718d2a9ccf3f88a304e6bcca03d72905c6
YOur name <binhqd.fami@gmail.com>
2015-09-11 21:13:23 +0700

    update branch

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 /* Workout */
 app.use('/', express.static(__dirname + '/public'));
 
+/* Listen */
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("listen" + port);
